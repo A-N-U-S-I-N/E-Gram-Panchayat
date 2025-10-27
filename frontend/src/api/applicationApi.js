d3fce13e19b41d3191f3cef955a42e20013d24d9
Anushka Singh <144716175+A-N-U-S-I-N@users.noreply.github.com>
2025-10-28 00:40:06 +0530

    Update applicationApi.js

--- frontend/src/api/applicationApi.js
+++ frontend/src/api/applicationApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/applications';
+const API_URL = `${process.env.REACT_APP_API_URL}/api/applications`;
 
 export const applyForService = async (payload, token) => {
   const response = await fetch(`${API_URL}`, { 
